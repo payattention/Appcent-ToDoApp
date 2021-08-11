5d9ee4289b6b40cf1e3273ff69e3ba2ea30229d1
ErkutKaralar <erkut_smac@hotmail.com>
2021-08-11 20:11:15 +0300

    nearly done.

--- ToDoApp.Tests/LoginTests.cs
+++ ToDoApp.Tests/LoginTests.cs
@@ -5,8 +5,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ToDoApp.ApiContract.Contracts;
 using ToDoApp.ApplicationService.Communicator.UserToDo;
 using ToDoApp.ApplicationService.Communicator.UserToDo.Model;
+using ToDoApp.Domain.UserToDoModels;
 using ToDoApp.Repository;
 
 namespace ToDoApp.Tests
@@ -14,18 +16,44 @@ namespace ToDoApp.Tests
     [TestFixture]
     public class LoginTests
     {
+        private Mock<IUserCouchbaseRepository> _iUserCouchbaseRepository;
+        private UserToDoCommunicator _userToDoCommunicator;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _iUserCouchbaseRepository = new Mock<IUserCouchbaseRepository>();
+            _userToDoCommunicator = new UserToDoCommunicator(_iUserCouchbaseRepository.Object);
+
+        }
+
         [Test]
         public async Task Login_CorrectPasswordUserName_ReturnLogged()
         {
-
             // Arrange
 
             string expectedUserName = "Erkut";
             string expectedPassword = "test123";
             LoginToDoRequestModel LoginModel = new LoginToDoRequestModel() { UserName = expectedUserName, Password = expectedPassword };
-            Mock<IUserCouchbaseRepository> _userCouchbaseRepo = new Mock<IUserCouchbaseRepository>();
-            UserToDoCommunicator _userToDoCommunicator = new UserToDoCommunicator(_userCouchbaseRepo.Object);
 
+            //Mock<IUserCouchbaseRepository> _userCouchbaseRepo = new Mock<IUserCouchbaseRepository>();
+            //UserToDoCommunicator _userToDoCommunicator = new UserToDoCommunicator(_userCouchbaseRepo.Object);
+
+             _iUserCouchbaseRepository.Setup(s => s.Login(new LoginToDoReqEntityModel()
+            {
+                UserName = "Erkut",
+                Password = "test123"
+            })).ReturnsAsync(new ResponseBase<LoginToDoResEntityModel>()
+            {
+                Success = true,
+                Data = new LoginToDoResEntityModel()
+                {
+                    Token = "123"
+                },
+                Message = "asd",
+                MessageCode = "11",
+                UserMessage = "trty"
+            });
             // Act
 
             var LoginResponse = await _userToDoCommunicator.Login(LoginModel);
